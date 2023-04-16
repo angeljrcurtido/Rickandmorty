87cac36f1c477ede2b3b2fdf05d8a6f4d48a482d
Angel Junior Curtido Quiñonez <106999344+angeljrcurtido@users.noreply.github.com>
2023-04-16 15:06:00 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import ConexionApi from './componente/conexionapi';
+function App() {
+  return (
+    <div className="App" >
+      <ConexionApi/>
+    </div>
+  );
+}
+
+export default App;
